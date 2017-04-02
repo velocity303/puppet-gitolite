84ca1960d5c99f56b47bc7e776e76131671213a6
James E. Jones <velocity303@gmail.com>
2017-04-02 01:42:46 -0500

    update to set users for gitolite install

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,28 +2,37 @@ class gitolite (
   $admin_key_content,
 ) {
 
+  user { 'git':
+    ensure => present,
+    group  => 'git',
+  }
+
   vcsrepo { '/opt/gitolite':
     ensure   => present,
+    user     => 'git',
     provider => git,
     source   => 'git://github.com/sitaramc/gitolite',
-  } 
+  }
 
   exec { 'install_gitolite':
     command => '/opt/gitolite/install -ln /usr/local/bin',
+    user    => 'git'
     path    => $::path,
     require => Vcsrepo['/opt/gitolite'],
   }
 
   file { '/tmp/admin_key.pub':
-    ensure => present,
+    ensure  => present,
     content => $admin_key_content,
     before  => Exec['setup_gitolite'],
   }
 
   exec { 'setup_gitolite':
-    command => 'gitolite setup -pk /tmp/admin_key.pub',
-    path    => $::path,
-    require => Exec['install_gitolite']
+    command     => 'gitolite setup -pk /tmp/admin_key.pub',
+    environment => 'HOME=$HOME'
+    user        => 'git'
+    path        => $::path,
+    require     => Exec['install_gitolite']
   }
 
 }
